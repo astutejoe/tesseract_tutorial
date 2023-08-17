93e467933a7eec84341ed08f694f00545397b29d
lukaszmalinowski14 <lukaszmalinowski14@gmail.com>
2023-08-17 13:51:31 +0200

    "DFSDF"

---
+++ run.py
@@ -0,0 +1,3 @@
+import os
+os.environ["TESSDATA_PREFIX"] = "C:/Python/ocr_10/tesseract_tutorial/tesseract/tessdata make training MODEL_NAME=Courier START_MODEL=eng TESSDATA=C:/Python/ocr_10/tesseract_tutorial/tesseract/tessdata MAX_ITERATIONS=100"
+print("END")
--- split_training_text.py
+++ split_training_text.py
@@ -7,11 +7,12 @@ training_text_file = 'langdata/eng.training_text'
 
 lines = []
 
-with open(training_text_file, 'r') as input_file:
+with open(training_text_file, 'r', encoding="utf-8") as input_file:
     for line in input_file.readlines():
         lines.append(line.strip())
 
-output_directory = 'tesstrain/data/Apex-ground-truth'
+# output_directory = 'tesstrain/data/Apex-ground-truth'
+output_directory = 'tesstrain/data/CourierETT-ground-truth'
 
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
@@ -19,21 +20,21 @@ if not os.path.exists(output_directory):
 random.shuffle(lines)
 
 count = 100
-
 lines = lines[:count]
-
 line_count = 0
 for line in lines:
     training_text_file_name = pathlib.Path(training_text_file).stem
-    line_training_text = os.path.join(output_directory, f'{training_text_file_name}_{line_count}.gt.txt')
-    with open(line_training_text, 'w') as output_file:
+    line_training_text = os.path.join(
+        output_directory, f'{training_text_file_name}_{line_count}.gt.txt')
+    with open(line_training_text, 'w', encoding="utf-8") as output_file:
         output_file.writelines([line])
 
     file_base_name = f'eng_{line_count}'
 
     subprocess.run([
         'text2image',
-        '--font=Apex',
+        # '--font=Apex',
+        '--font=CourierETT',
         f'--text={line_training_text}',
         f'--outputbase={output_directory}/{file_base_name}',
         '--max_pages=1',
